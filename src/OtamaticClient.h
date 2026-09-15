dfcb46614aeac8cacbd608d956b4adeaa8d18a71
Timothy Franceschi <timothy@franceschi.es>
2026-09-15 10:30:44 +0200

    Removed obsolete private variable _fetchPath

--- src/OtamaticClient.h
+++ src/OtamaticClient.h
@@ -119,7 +119,6 @@ private:
 
     const char* _serverHost = "192.168.1.17";
     uint16_t _serverPort = 8000;
-    const char* _fetchPath = "/api/v1/ota/download";
 
     uint32_t _checkInterval = 5 * 60 * 1000;
     uint32_t _lastCheck = 0;
